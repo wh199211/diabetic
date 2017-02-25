d84cadc0d439b79a690c2655eeaf02405a30753b
haowang <wh19921130@gmail.com>
2017-02-26 00:10:58 +0800

    commit

---
+++ generator.py
@@ -0,0 +1,12 @@
+from scipy.misc import imread
+import os
+import pandas as p
+
+class generator:
+	def __init__(self):
+		#TODO
+	def train_gen(self, img_path, label_path, batch_size=64):
+		#TODO
+	
+	def test_gen(self):
+		#TODO
